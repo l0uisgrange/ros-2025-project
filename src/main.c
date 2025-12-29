4c0ab5337d38db366045d9b04594bfa95dd01ea1
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2025-12-29 12:00:35 +0100

    Refactor main.c for sensor integration and cleanup
    Refactored main.c to include new sensor and message bus functionalities, and removed unused includes and comments.

--- src/main.c
+++ src/main.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <leds.h>
 #include <ch.h>
 #include <hal.h>
 #include <memory_protection.h>
 #include <usbcfg.h>
-#include <motors.h>
-#include <camera/po8030.h>
+#include "motors.h"
 #include <chprintf.h>
-
-// In order to be able to use the RGB LEDs and User button
-// These funtcions are handled by the ESP32 and the communication with the uC is done via SPI
-#include <spi_comm.h>
+#include "sensors/imu.h"
+#include "sensors/proximity.h"
+#include "msgbus/messagebus.h"
 
 #include "main.h"
-#include "pi_regulator.h"
-#include "process_image.h"
+#include "travel.h"
+#include "detection.h"
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
-{
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+void SendUint8ToComputer(uint8_t* data, uint16_t size) {
 	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
 	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
 	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
 }
 
-static void serial_start(void)
-{
+static void serial_start(void) {
 	static SerialConfig ser_cfg = {
 	    115200,
 	    0,
@@ -37,67 +38,59 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-int main(void)
-{
+int main(void) {
     halInit();
     chSysInit();
     mpu_init();
 
-    //starts the serial communication
+    // Starts the serial communication
     serial_start();
-    //start the USB communication
-    usb_start();
-    //starts the camera
-    dcmi_start();
-	po8030_start();
-	//inits the motors
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+    chThdSleepMilliseconds(100);
+
+	// Shows LED for configuration steps
+	set_rgb_led(LED2, 0, 255, 0);
+	set_rgb_led(LED4, 0, 255, 0);
+	set_rgb_led(LED6, 0, 255, 0);
+	set_rgb_led(LED8, 0, 255, 0);
+	set_led(LED2, true);
+
+	// Configures and enables the IMU and proximity (IR) sensors
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 1/6] Starting sensors\r\n");
+	imu_start();
+	proximity_start();
+	set_led(LED4, true);
+
+    // Waits 2s to be sure the e-puck is in a stable position
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 2/6] Waiting stabilization\r\n");
+    chThdSleepMilliseconds(2000);
+	set_led(LED6, true);
+
+	// IMU and proximity sensors Calibration
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 3/6] Calibrating sensors\r\n");
+    calibrate_acc();
+	calibrate_ir();
+	set_led(LED8, true);
+
+	// Inits and enables the motors
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 4/6] Starting motors\r\n");
 	motors_init();
-
-	//starts RGB LEDS and User button managment
-	spi_comm_start();
-
-	//init color detection mode: see process_image.h for values
-	//the color detection can be controled from plotImage Python code
-	select_color_detection(GREEN_COLOR);
-
-	//enable motors by default. Can be chnaged from plotImage Python code
 	set_enabled_motors(true);
 
-	//stars the threads for the pi regulator and the processing of the image
-	pi_regulator_start();
-	process_image_start();
+	// Starts the threads
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 5/6] Starting regulators\r\n");
+	travel_thread_start();
+	wall_detection_start();
 
-    /* Infinite loop: used here for handling control commands sent from plotImage Python code */
-    while (1) {
-		// read control commands
-        volatile uint8_t ctrl_cmd = chSequentialStreamGet((BaseSequentialStream *) &SD3); // Fonction bloquante
-		switch (ctrl_cmd) {
-			case 'R':
-			case 'r':
-				select_color_detection(RED_COLOR);
-				break;
-			case 'G':
-			case 'g':
-				select_color_detection(GREEN_COLOR);
-				break;
-			case 'B':
-			case 'b':
-				select_color_detection(BLUE_COLOR);
-				break;
-			case 'M':
-			case 'm':
-				toogle_enabled_motors();
-				break;
-		}
-    	//waits 0.2 second
-		chThdSleepMilliseconds(200);
-    }
+	chprintf((BaseSequentialStream *)&SD3, "[INIT 6/6] Finished initialisation\r\n");
+	set_rgb_led(LED4, 0, 255, 0);
+	clear_leds();
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-void __stack_chk_fail(void)
-{
+void __stack_chk_fail(void) {
     chSysHalt("Stack smashing detected");
 }
